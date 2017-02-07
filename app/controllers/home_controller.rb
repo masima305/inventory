ea68f3a85e2276eb1f070d88c28d6325ac24caf3
YouTaek <jyt9319@likelion.org>
2017-02-07 08:35:24 +0000

    for keroku

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,29 @@
+class HomeController < ApplicationController
+  def index
+    
+  end
+
+  def write
+    
+  end
+  
+  #function index
+  
+  def matching_model
+    #모델을 불러오고, 해쉬 어레이에 저장.
+  end
+  
+  def finding (key)
+    #해쉬에 저장된 인덱스와 키워드 비교
+    return output(key)
+  end
+  
+  def outputer (output)
+    # finding에서 나온 값으로 다시 어레이 생성.
+    return out_arr[]
+  end
+  
+  def lister(out_arr[])
+    # 
+  end   
+end
